f5a13f0a1ec2263e596aeae75b877f8dc0e22a6b
Daisy Smith <daisy_madonna808@Daisy-Smiths-MacBook-Pro.local>
2014-07-24 11:36:45 +1000

    form hiding  hiding and showing not completed yet

--- app/assets/javascripts/responsiveSettings.js
+++ app/assets/javascripts/responsiveSettings.js
@@ -1,18 +1,38 @@
 $(document).ready(function () {
-  // $('#settings').on('select' function () {
-  //   document.getElementById('visualisers')
-  // })
+    var getTimeDomain = function() {
+    return Sound.getTimeDomain();
+  };
 
-  // $('#visualisers_list').val().on('select' function () {
+  var getFrequencies = function() {
+    return Sound.getFrequencyDomain();
+  };
+  var currentAnimationId;
+
+  function stopPrevious () {
+    try{
+      cancelAnimationFrame(currentAnimationId);
+      $('#container').empty();
+    } catch(err) {
+      console.log('no id');
+    }
+  };
 
   $( "#linesSpeedControls").hide();
+  $( "#sunflareControls").hide();
+  $( "#cubeGridControls").hide();
 
-  if ($( "#visualisers_list option:selected" ).val() == 'lines'){
-    linesSettings();
-  }
-
-
-  var linesSettings = function (){
-    $('#linesSpeedControls').show();
-  };
+  $( "#visualisers_list" ).on('change', function(){
+    console.log('changing');
+    if ($( "#visualisers_list option:selected" ).val() == 'lines'){$('#linesSpeedControls').show(); $( "#sunflareControls").hide();}
+    if ($( "#visualisers_list option:selected" ).val() == 'sunflare'){$('#sunflareControls').show(); $( "#linesSpeedControls").hide(); loadSunflare();}
+    if ($( "#visualisers_list option:selected" ).val() == 'emitter'){$( "#linesSpeedControls").hide(); $( "#sunflareControls").hide();}
+  })
 });
+
+var loadSunflare = function (){
+    $('#sunflareSubmit').on('click',function(){
+    console.log(currentAnimationId);
+    stopPrevious();
+    sunFlare(getTimeDomain, getFrequencies);
+    });
+  };
